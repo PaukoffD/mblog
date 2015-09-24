43c012afe56765f81b9e26bf3300fa2c97d3cd4e
xuliaP <paukov2967@gmail.com>
2015-09-24 14:55:12 +0300

    devise and redcarpet
    devise and redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,8 @@
 module ApplicationHelper
 def markdown(text)
     options = {
-      filter_html:     true,
-      hard_wrap:       true, 
-      link_attributes: { rel: 'nofollow', target: "_blank" },
-      space_after_headers: true, 
-      fenced_code_blocks: true
-    }
+            space_after_headers: true, 
+          }
 
     extensions = {
       autolink:           true,
